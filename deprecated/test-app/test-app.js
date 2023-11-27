0fae1ba50ed77a5a819e0a95d32b27fd878df62e
jasonofthestorm <jgluck@gmail.com>
2023-11-27 02:27:20 -0800

    no message

--- deprecated/test-app/test-app.js
+++ deprecated/test-app/test-app.js
@@ -37,8 +37,7 @@ $(function() {
     //
 
     // create the socket to the local OSC server
-    // var socket = new io.Socket("master--elaborate-faun-bfb800.netlify.app", { port: 80, rememberTransport: false });
-    var socket = io('https://master--elaborate-faun-bfb800.netlify.app');
+    var socket = new io.Socket("master--elaborate-faun-bfb800.netlify.app", { port: 80, rememberTransport: false });
     
     // NOTE: we can create sockets to remote hosts too!!!
 
